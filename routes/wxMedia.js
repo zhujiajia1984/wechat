7b9a71e6f669891a63094b6378c3c708f041b064
zhujiajia <772978409@qq.com>
2018-04-15 15:48:21 +0800

    获取素材列表图片

--- routes/wxMedia.js
+++ routes/wxMedia.js
@@ -1,5 +1,6 @@
 /*
 	微信公众号素材管理
+	获取素材列表：get http://wechat.weiquaninfo.cn/wxMedia/getMediaList
 */
 
 var express = require('express');
@@ -7,12 +8,68 @@ var router = express.Router();
 var logger = require('../logs/log4js').logger;
 var redisClient = require('../redis');
 var https = require('https');
+var searchJson = {
+	type: "image",
+	offset: 0,
+	count: 20
+}
 
 //////////////////////////////////////////////////////////////////////////
 // 获取永久素材列表
 router.get('/getMediaList', function(req, res, next) {
-	res.send("media");
+	getAccessToken().then((access_token) => {
+		return getMediaList(access_token);
+	}).then((result) => {
+		res.status(200).send(result);
+	}).catch((error) => {
+		logger.error(error);
+		res.status(417).send("error");
+	})
 });
+
+//////////////////////////////////////////////////////////////////////////
+// 获取access_token
+function getAccessToken() {
+	return new Promise((resolve, reject) => {
+		redisClient.get('token', (error, access_token) => {
+			if (error) return reject(error);
+			access_token = access_token.replace(/\"/g, "");
+			return resolve(access_token);
+		})
+	})
+}
+
+//////////////////////////////////////////////////////////////////////////
+// 获取素材列表
+function getMediaList(access_token) {
+	return new Promise((resolve, reject) => {
+		const postData = JSON.stringify(searchJson);
+		const options = {
+			hostname: "api.weixin.qq.com",
+			path: `/cgi-bin/material/batchget_material?access_token=${access_token}`,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(postData, 'utf-8')
+			}
+		};
+		const req = https.request(options, (res) => {
+			res.setEncoding('utf8');
+			let rawData = '';
+			res.on('data', (chunk) => { rawData += chunk; });
+			res.on('end', () => {
+				// 反馈结果
+				// logger.info("rawData:", rawData);
+				resolve(JSON.parse(rawData));
+			});
+		});
+		req.on('error', (e) => {
+			reject(`problem with request: ${e.message}`);
+		});
+		req.write(postData);
+		req.end();
+	})
+}
 
 //
 module.exports = router;
