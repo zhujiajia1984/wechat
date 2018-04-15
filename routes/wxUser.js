bf29f28f46a3a4d5e275eea1001fa354e94cb558
zhujiajia <772978409@qq.com>
2018-04-15 22:02:34 +0800

    获取已关注用户列表

--- routes/wxUser.js
+++ routes/wxUser.js
@@ -1,6 +1,7 @@
 /*
 	微信公众号用户管理
 	获取用户基本信息：get http://wechat.weiquaninfo.cn/wxUser/getUserInfo
+	获取关注用户列表：get http://wechat.weiquaninfo.cn/wxUser/getUserList
 */
 
 var express = require('express');
@@ -11,12 +12,30 @@ var https = require('https');
 
 // const
 const openId = "oMBhJ0tdCdBtY07FXuzsywkEyU6A";
+const openId2 = "oMBhJ0tF0gZBDx0aOSvdY8DW3qoE";
+const openId3 = "oMBhJ0pTWmx8kTAY7aV4FDGAXVzM";
+const openId4 = "oMBhJ0q6xj0m-FICszr6UezwJjPE";
+const openId5 = "oMBhJ0pSghlN3w_WC237w-Hecwn4";
+const openId6 = "oMBhJ0mE7W9p_MLpMihh9DdMdOPw";
 
 //////////////////////////////////////////////////////////////////////////
 // 获取用户基本信息
 router.get('/getUserInfo', function(req, res, next) {
 	getAccessToken().then((access_token) => {
 		return getUserInfo(access_token);
+	}).then((result) => {
+		res.status(200).send(result);
+	}).catch((error) => {
+		logger.error(error);
+		res.status(417).send(error);
+	})
+});
+
+//////////////////////////////////////////////////////////////////////////
+// 获取关注用户列表
+router.get('/getUserList', function(req, res, next) {
+	getAccessToken().then((access_token) => {
+		return getUserList(access_token);
 	}).then((result) => {
 		res.status(200).send(result);
 	}).catch((error) => {
@@ -42,6 +61,32 @@ function getAccessToken() {
 function getUserInfo(access_token) {
 	return new Promise((resolve, reject) => {
 		let url = `https://api.weixin.qq.com/cgi-bin/user/info?access_token=${access_token}&openid=${openId}&lang=zh_CN`;
+		https.get(url, (res) => {
+			res.setEncoding('utf8');
+			let rawData = '';
+			res.on('data', (chunk) => { rawData += chunk; });
+			res.on('end', () => {
+				// 结果
+				let result = JSON.parse(rawData);
+				if (typeof(result.errcode) == "undefined") {
+					// 获取成功
+					// logger.info(result);
+					resolve(result);
+				} else {
+					reject(result);
+				}
+			});
+		}).on('error', (e) => {
+			return reject(e);
+		});
+	})
+}
+
+//////////////////////////////////////////////////////////////////////////
+// 获取关注用户列表
+function getUserList(access_token) {
+	return new Promise((resolve, reject) => {
+		let url = `https://api.weixin.qq.com/cgi-bin/user/get?access_token=${access_token}&next_openid=`;
 		https.get(url, (res) => {
 			res.setEncoding('utf8');
 			let rawData = '';
